0f386e4bfbe2f96e3f16c9b64635ef13385aa142
Jacob Williams <jacob.williams@psycle.com>
2025-02-05 15:31:09 +0000

    linting

--- src/Components/UI/Header/Header.js
+++ src/Components/UI/Header/Header.js
@@ -1,5 +1,5 @@
 import styles from "./Header.module.css";
-import personalStyles from "../PersonalInfo.module.css";
+import personalStyles from "../PersonalInfo/PersonalInfo.module.css";
 import {
   BsFacebook,
   BsLinkedin,
